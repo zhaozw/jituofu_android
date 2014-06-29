be2c8c491e806dd7ebebc1c9488b0b6011a13d56
RememberGrace-dev <253456273@qq.com>
2014-06-29 17:59:30 +0800

    完成基本的父分类查询功能

--- 2.0/src/com/jituofu/ui/UIParentType.java
+++ 2.0/src/com/jituofu/ui/UIParentType.java
@@ -1,17 +1,26 @@
 package com.jituofu.ui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
-import android.app.Notification.Builder;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.jituofu.R;
@@ -20,14 +29,16 @@ import com.jituofu.base.BaseUiAuth;
 import com.jituofu.base.C;
 import com.jituofu.util.AppUtil;
 
-public class UIParentType extends BaseUiAuth {
+public class UIParentType extends BaseUiAuth implements OnClickListener {
 	private Bundle extraBundle;
 
 	private LinearLayout topbarView, noDataView, againView;
 
 	private ImageButton addView, editView;
 
-	private TextView titleView;
+	private TextView titleView, noDataAddTypeView;
+
+	private ListView lv;
 
 	// 页面来源
 	private String from;
@@ -35,6 +46,11 @@ public class UIParentType extends BaseUiAuth {
 	// 查询分类相关
 	private int pageNum = 1;
 	private boolean initQuery = false;// 是首次查询还是分页查询
+	private ArrayList<HashMap<String, String>> datList = new ArrayList<HashMap<String, String>>();
+
+	private CustomAdapter customAdapter;
+
+	private boolean isRefresh, isLoadMore;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +62,7 @@ public class UIParentType extends BaseUiAuth {
 		bindListener2View();
 
 		// 查询分类
-		initQuery = true;//标记初始化查询为true
+		initQuery = true;// 标记初始化查询为true
 		AppUtil.showLoadingPopup(this, R.string.SPFL_QUERY_LOADING);
 		doQueryTask();
 	}
@@ -78,6 +94,8 @@ public class UIParentType extends BaseUiAuth {
 		if (resultStatus == 100) {
 			switch (taskId) {
 			case C.TASK.gettypes:
+				JSONArray types = operation.getJSONArray("types");
+				renderList(types);
 				break;
 			}
 		} else {
@@ -85,8 +103,39 @@ public class UIParentType extends BaseUiAuth {
 		}
 	}
 
+	private void renderList(JSONArray data) throws JSONException {
+		// 初始化加载时，没有数据显示无数据提示
+		if (initQuery && data.length() <= 0) {
+			lv.setVisibility(View.GONE);
+			noDataView.setVisibility(View.VISIBLE);
+			((TextView) noDataView.findViewById(R.id.txt))
+					.setText(R.string.SPFL_QUERY_NODATA);
+			((TextView) noDataView.findViewById(R.id.action_btn))
+					.setText(R.string.SPFL_QUERY_ADDTXT);
+
+			initQuery = false;
+			return;
+		}
+
+		for (int i = 0; i < data.length(); i++) {
+			HashMap<String, String> map = new HashMap<String, String>();
+			JSONObject json = data.getJSONObject(i);
+			map.put("name", json.getString("name"));
+			map.put("id", json.getString("id"));
+			datList.add(map);
+		}
+
+		customAdapter = customAdapter == null ? new CustomAdapter(this,
+				datList, R.layout.template_types_item, new String[] { "name" },
+				new int[] { R.id.txt }) : customAdapter;
+		lv.setAdapter(customAdapter);
+	}
+
 	private void bindListener2View() {
 		this.onCustomBack();
+		noDataAddTypeView.setOnClickListener(this);
+		((TextView) againView.findViewById(R.id.again_btn))
+				.setOnClickListener(this);
 	}
 
 	private void initView() {
@@ -98,6 +147,10 @@ public class UIParentType extends BaseUiAuth {
 
 		noDataView = (LinearLayout) this.findViewById(R.id.noData);
 		againView = (LinearLayout) this.findViewById(R.id.again);
+
+		lv = (ListView) this.findViewById(R.id.listView);
+
+		noDataAddTypeView = (TextView) noDataView.findViewById(R.id.action_btn);
 	}
 
 	@SuppressWarnings("deprecation")
@@ -125,5 +178,78 @@ public class UIParentType extends BaseUiAuth {
 					R.drawable.edit_small));
 		}
 
+	}
+
+	/**
+	 * 自定义适配器
+	 * 
+	 * @author zhuqi
+	 * 
+	 */
+	class CustomAdapter extends SimpleAdapter {
+
+		public CustomAdapter(Context context,
+				List<? extends Map<String, ?>> data, int resource,
+				String[] from, int[] to) {
+			super(context, data, resource, from, to);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = super.getView(position, convertView, parent);
+				holder = new ViewHolder();
+				holder.txt = (TextView) convertView.findViewById(R.id.txt);
+				holder.delete = (ImageView) convertView
+						.findViewById(R.id.delete);
+				holder.edit = (ImageView) convertView.findViewById(R.id.edit);
+				// 保存视图项
+				convertView.setTag(holder);
+			} else {
+				// 取出现有的视图项
+				holder = (ViewHolder) convertView.getTag();
+			}
+
+			return convertView;
+		}
+	}
+
+	static class ViewHolder {
+		ImageView delete, edit;
+		TextView txt;
+	}
+
+	@Override
+	public void onClick(View v) {
+		int vId = v.getId();
+
+		switch (vId) {
+		case R.id.action_btn:
+			break;
+		case R.id.again_btn:
+			break;
+		}
+	}
+
+	public void onNetworkError(int taskId) {
+		this.showToast(C.ERROR.networkException);
+	}
+
+	public void onNetworkTimeout(int taskId) {
+		this.showToast(C.ERROR.networkTimeout);
+	}
+
+	public void onServerException(int taskId) {
+		if (initQuery) {
+			lv.setVisibility(View.GONE);
+			againView.setVisibility(View.VISIBLE);
+			((TextView) againView.findViewById(R.id.txt))
+					.setText(C.ERROR.serverException);
+
+			initQuery = false;
+			return;
+		}
 	}
 }
