d42de08579051ee1faa485206f9623e65e539215
RememberGrace-dev <253456273@qq.com>
2014-07-15 15:15:47 +0800

    准备使用slidingmenu组件

--- 2.0/src/com/jituofu/ui/UICashier.java
+++ 2.0/src/com/jituofu/ui/UICashier.java
@@ -1,36 +1,50 @@
 package com.jituofu.ui;
 
 import com.jituofu.R;
-import com.jituofu.base.BaseUi;
 import com.jituofu.base.BaseUiAuth;
 import com.jituofu.base.BaseUiFormBuilder;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TabHost;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 public class UICashier extends BaseUiAuth implements BaseUiFormBuilder {
-
+	private ImageButton cangkuBtnView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.page_cashier);
+		
+		initView();
+		onBind();
+	}
+	
+	private void onBind(){
+		cangkuBtnView.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+	}
+	
+	private void initView(){
+		cangkuBtnView = (ImageButton) this.findViewById(R.id.cangku);
 	}
 
 	@Override
 	public void doSubmit() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void beforeSubmit() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
